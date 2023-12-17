9753386f499b4a03b8f294ecd5f00b52f8fc1a12
johnfan12 <130122089+johnfan12@users.noreply.github.com>
2023-12-17 19:39:55 +0800

    update

--- scripts/random_comment.js
+++ scripts/random_comment.js
@@ -1,5 +1,5 @@
 function showcomment() {
-var commentIds = ["杨盼盼", "周经美", "全思", "龚贤武", "朱进玉", "张维为", "Wilson Edwards"];
+var commentIds = ["杨盼盼", "周经美", "全思", "龚贤武", "朱进玉", "张维为", "Wilson Edwards","李登峰"];
 var comments = [
     "博主分析的好透測，坐等看长安大学未來的狀況，希望可以發展成一流大学",
     "博主的分析很有啟發！",
